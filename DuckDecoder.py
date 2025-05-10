302c38893f480a405a49a0448e5be6b57e5e1372
Anaël Messan <74980186+capocollo@users.noreply.github.com>
2025-05-11 00:03:17 +0200

    Update DuckDecoder.py
    Added support for capitalization (left shift key)

--- DuckDecoder.py
+++ DuckDecoder.py
@@ -30,10 +30,14 @@ def letiscover(letters, types, mode):
         "04", "05", "06", "07", "08", "09", "0a", "0b", "0c", "0d", "0e", "0f", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "1a", "1b", "1c", "1d", "36", "37", "38", "33", "34", "2f", "30", "31", "2d", "2e", "2c", "28", "1e", "1f", "20", "21", "22", "23", "24", "25", "26", "27", "2a", "35", "2b", "52", "51", "4f", "50", "4c"
     ]
     
-    for i in range(len(letters)):
+    for i in range(len(letters)-1):
         if letters[i] in HexLetters:
-            LetterPos = HexLetters.index(letters[i])
-            Result.append(Letters[LetterPos])
+            if letters[i+1] == "02":
+                LetterPos = HexLetters.index(letters[i])
+                Result.append(CapLetters[LetterPos])
+            else :
+                LetterPos = HexLetters.index(letters[i])
+                Result.append(Letters[LetterPos])
     
     return Result
 
@@ -52,7 +56,7 @@ if __name__ == "__main__":
     filename = os.path.realpath(sys.argv[2])
     
     List = dsem(hexstr(filename), 2)
-    chars = List[::2]
+    chars = List[::1]
     types = List[1::2]
     
     if mode == "decode":
